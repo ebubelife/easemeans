9ca80b91f80786a36e4557f40de97846e12f929a
ebubelife <ebubeemeka19@gmail.com>
2024-11-04 11:57:42 +0100

    swapped out new cred

--- app/Http/Controllers/SafeHaven.php
+++ app/Http/Controllers/SafeHaven.php
@@ -23,7 +23,7 @@ class SafeHaven extends Controller
         $url = 'https://api.safehavenmfb.com/oauth2/token';
 
         $headers = [
-            'ClientID' => '8b7783aa56bed8fd48e4cdfba5409423',
+        
             'accept' => 'application/json',
             'content-type' => 'application/json',
             
@@ -34,7 +34,7 @@ class SafeHaven extends Controller
       
         'grant_type'=> 'client_credentials',
         'client_assertion_type' => 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer',
-        'client_id' => '687f506bb556c185217357deeb27b156',
+        'client_id' => '387cbcfe86b75f0abc54b579cb49feff',
         'client_assertion' => 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL2Vhc2VtZWFucy5jb20iLCJzdWIiOiI2ODdmNTA2YmI1NTZjMTg1MjE3MzU3ZGVlYjI3YjE1NiIsImF1ZCI6Imh0dHBzOi8vYXBpLnNhZmVoYXZlbm1mYi5jb20iLCJpYXQiOjE3MjYyNzE1MjAsImV4cCI6MTgyMjM0ODc5OX0.ZNoqmU21Jbh-XDB_cSiYoi-9sVxhzkDmAjMqF02giV5G4E01umZrZbDx6bfaD3r5YDhP8IMB24F36MtTjz75KG4Q_lA_tdVjsmsiL9LsFRuqsFjzF_VTPdVt-ZHtSuBbdnfsMV81OAE-Tfxxph4J41LT7HpIy8zBsiwQytyyrMQ',
 
        
