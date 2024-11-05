198f3e1a1a3c608da92c174c7e58ca227760902a
ebubelife <ebubeemeka19@gmail.com>
2024-11-05 08:29:51 +0100

    ...

--- app/Http/Controllers/VirtualAccountsController.php
+++ app/Http/Controllers/VirtualAccountsController.php
@@ -171,7 +171,7 @@ class VirtualAccountsController extends Controller
      
   
          // POST request using the created object
-         $postResponse = $client->post($url, [
+         $postResponse = $client->get($url, [
              'headers' => $headers,
             
          ]);
