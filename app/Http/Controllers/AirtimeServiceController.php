eded2143d378743f276f31a08a1649489d42a8d8
ebubelife <ebubeemeka19@gmail.com>
2024-11-16 12:59:26 +0100

    ...

--- app/Http/Controllers/AirtimeServiceController.php
+++ app/Http/Controllers/AirtimeServiceController.php
@@ -131,7 +131,7 @@ class AirtimeServiceController extends Controller
          $transaction->service = "AIRTIME";
          $transaction->category = $validated["network"];
          $transaction->amount = $validated["amount"];
-         $transaction->tx_detail = $decodedBody["data"];
+         $transaction->tx_detail = json_encode($decodedBody["data"]);
          $transaction->status = "SUCCESS";
          $transaction->save();
 
